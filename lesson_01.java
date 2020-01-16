a38d7180fefd257542dc2d1fa4fd06e236070ac1
Evgeniya Khyut <eghikhyut@mail.ru>
2020-01-17 01:32:25 +0300

    Добавила комментарии и имя

--- lesson_01.java
+++ lesson_01.java
@@ -1,6 +1,9 @@
+// author Евгения Хют
 package ru.geekbrains;
 
 public class lesson_01 {
+    // Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат
+    // с плавающей точкой, где a, b, c, d – целочисленные входные параметры этого метода
     public static double calculate(int a, int b, int c, int d) {
         float af = (float) a;
         float bf = (float) b;
@@ -10,6 +13,8 @@ public class lesson_01 {
         return af * (bf + (cf / df));
     }
 
+    // Написать метод, принимающий на вход два целых числа, и проверяющий что их сумма лежит
+    // в пределах от 10 до 20(включительно), если да – вернуть true, в противном случае – false
     public static boolean isInLimits(int a, int b) {
         int sum = a + b;
 
@@ -20,6 +25,9 @@ public class lesson_01 {
         }
     }
 
+    // Написать метод, которому в качестве параметра передается целое число, метод должен проверить
+    // положительное ли число передали, или отрицательное.
+    // Замечание: ноль считаем положительным числом. Результат работы метода вывести в консоль.
     public static void isPositive(int a) {
         if (a < 0) {
             System.out.println("Your number is negative!");
@@ -28,29 +36,37 @@ public class lesson_01 {
         }
     }
 
+    // Написать метод, которому в качестве параметра передается строка, обозначающая имя,
+    // метод должен вернуть приветственное сообщение «Привет, переданное_имя!»; вывести приветствие в консоль.
     public static void greet(String name) {
         System.out.println("Привет, " + name + "!");
     }
 
+    // *Написать метод, который определяет является ли год високосным. Каждый 4-й год является високосным,
+    // кроме каждого 100-го, при этом каждый 400-й – високосный.
+    // Для проверки работы вывести результаты работы метода в консоль.
     public static void isLeap(int year) {
-        /*if (year % 4 == 0) {
-           *//* if (year % 400 == 0) {
-                System.out.println(year + " is leap year");
-            } else if (year % 100 == 0) {
-                System.out.println(year + " is not leap year");
-            } else {
-                System.out.println(year + " is leap year");
-            }*//*
+        // Первая версия
+        // if (year % 4 == 0) {
+        //    /* if (year % 400 == 0) {
+        //         System.out.println(year + " is leap year");
+        //     } else if (year % 100 == 0) {
+        //         System.out.println(year + " is not leap year");
+        //     } else {
+        //         System.out.println(year + " is leap year");
+        //     }*/
 
-            if (year % 100 == 0 && year % 400 != 0) {
-                System.out.println(year + " is not leap year");
-            } else {
-                System.out.println(year + " is leap year");
-            }
-        } else {
-            System.out.println(year + " is not leap year");
-        }*/
+        //     // Вторая версия
+        //     if (year % 100 == 0 && year % 400 != 0) {
+        //         System.out.println(year + " is not leap year");
+        //     } else {
+        //         System.out.println(year + " is leap year");
+        //     }
+        // } else {
+        //     System.out.println(year + " is not leap year");
+        // }
 
+        // Финальная версия
         if (year % 4 == 0 && (year % 400 == 0 || year % 100 != 0)) {
             System.out.println(year + " is leap year");
         } else {
